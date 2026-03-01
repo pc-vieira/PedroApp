1bb9b9909fe9a2391450ee9a01899710b0c472a5
Pedro Vieira <pedrocostavieira7@gmail.com>
2026-03-01 17:05:30 +0000

    Update App.js with welcome message and downgrade Expo SDK to 54 for compatibility with Expo Go

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* This is the text you need for your assignment */}
+      <Text>Hi! my name is Pedro Vieira</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +14,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, 
+    backgroundColor: '#fff', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
   },
 });
